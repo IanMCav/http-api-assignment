a58dfded2056ad19adf4a5b9cdbe3741a629983c
Ian Cavanaugh <flameian@mac.com>
2017-09-21 06:53:31 -0400

    Build to circleCI?

--- src/server.js
+++ src/server.js
@@ -6,50 +6,50 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/success': jsonHandler.success,
-    '/badRequest': jsonHandler.badRequest,
-    notFound: jsonHandler.notFound,
-};
+/* const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  notFound: jsonHandler.notFound,
+}; */
 
 const handlePost = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/addUser') {
-        const res = response;
-        const body = [];
-        request.on('error', (err) => {
-            console.dir(err);
-            res.statusCode = 400;
-            res.end();
-        });
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        request.on('end', () => {
-            const bodyString = Buffer.concat(body).toString();
-            const bodyParams = query.parse(bodyString);
-            jsonHandler.addUser(request, res, bodyParams);
-        });
-    }
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+    const body = [];
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  }
 };
 
 const handleGet = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/style.css') {
-        htmlHandler.getCSS(request, response);
-    } else if (parsedUrl.pathname === '/getUsers') {
-        jsonHandler.getUsers(request, response);
-    } else {
-        htmlHandler.getIndex(request, response);
-    }
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
 };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    if (request.method === 'POST') {
-        handlePost(request, response, parsedUrl);
-    } else {
-        handleGet(request, response, parsedUrl);
-    }
+  const parsedUrl = url.parse(request.url);
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
 };
 
 http.createServer(onRequest).listen(port);

05483ed13d87943065f7d5f19567d973c5654dca
Ian Cavanaugh <flameian@mac.com>
2017-09-21 10:36:56 -0400

    Project complete

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,80 +1,102 @@
 const respondJSON = (request, response, status, object) => {
-  response.writeHead(status, {
-    'Content-Type': 'application/json',
-  });
-  response.write(JSON.stringify(object));
-  response.end();
+    response.writeHead(status, {
+        'Content-Type': 'application/json',
+    });
+    response.write(JSON.stringify(object));
+    response.end();
 };
 
 const success = (request, response) => {
-  const responseJSON = {
-    message: 'This is a successful response',
-  };
+    const responseJSON = {
+        message: 'This is a successful response',
+    };
 
-  respondJSON(request, response, 200, responseJSON);
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const badRequest = (request, response, params) => {
-  let responseJSON = 0;
-  let errCode = 0;
+    let responseJSON = 0;
+    let errCode = 0;
 
-  if (params === 'valid=true') {
-    responseJSON = { message: 'This request has the required parameters' };
-    errCode = 200;
-  } else {
-    responseJSON = { message: 'Missing valid query parameter set to true', id: 'badRequest' };
-    errCode = 400;
-  }
+    if (params === 'valid=true') {
+        responseJSON = {
+            message: 'This request has the required parameters'
+        };
+        errCode = 200;
+    } else {
+        responseJSON = {
+            message: 'Missing valid query parameter set to true',
+            id: 'badRequest'
+        };
+        errCode = 400;
+    }
 
-  respondJSON(request, response, errCode, responseJSON);
+    respondJSON(request, response, errCode, responseJSON);
 };
 
 const unauthorized = (request, response, params) => {
-  let responseJSON = 0;
-  let errCode = 0;
+    let responseJSON = 0;
+    let errCode = 0;
 
-  if (params === 'loggedIn=yes') {
-    responseJSON = { message: 'You have successfully viewed the content' };
-    errCode = 200;
-  } else {
-    responseJSON = { message: 'Missing loggedIn query paramater set to yes', id: 'unauthorized' };
-    errCode = 401;
-  }
+    if (params === 'loggedIn=yes') {
+        responseJSON = {
+            message: 'You have successfully viewed the content'
+        };
+        errCode = 200;
+    } else {
+        responseJSON = {
+            message: 'Missing loggedIn query paramater set to yes',
+            id: 'unauthorized'
+        };
+        errCode = 401;
+    }
 
-  respondJSON(request, response, errCode, responseJSON);
+    respondJSON(request, response, errCode, responseJSON);
 };
 
 const forbidden = (request, response) => {
-  const responseJSON = { message: 'You do not have access to this content', id: 'forbidden' };
+    const responseJSON = {
+        message: 'You do not have access to this content',
+        id: 'forbidden'
+    };
 
-  respondJSON(request, response, 403, responseJSON);
+    respondJSON(request, response, 403, responseJSON);
 };
 
 const internal = (request, response) => {
-  const responseJSON = { message: 'Internal Server Error. Something went wrong.', id: 'internalError' };
+    const responseJSON = {
+        message: 'Internal Server Error. Something went wrong.',
+        id: 'internalError'
+    };
 
-  respondJSON(request, response, 500, responseJSON);
+    respondJSON(request, response, 500, responseJSON);
 };
 
 const notImplemented = (request, response) => {
-  const responseJSON = { message: 'A get request for this page has not been implemented yet. Check again later for updated content.', id: 'notImplemented' };
+    const responseJSON = {
+        message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
+        id: 'notImplemented'
+    };
 
-  respondJSON(request, response, 501, responseJSON);
+    respondJSON(request, response, 501, responseJSON);
 };
 
 const phorohphor = (request, response) => {
-  const responseJSON = { message: 'the page you are looking for was not found.', id: 'notFound' };
+    const responseJSON = {
+        message: 'the page you are looking for was not found.',
+        id: 'notFound'
+    };
 
-  respondJSON(request, response, 404, responseJSON);
+    respondJSON(request, response, 404, responseJSON);
 };
 
 
 module.exports = {
-  success,
-  badRequest,
-  unauthorized,
-  forbidden,
-  internal,
-  notImplemented,
-  phorohphor,
+    success,
+    badRequest,
+    unauthorized,
+    forbidden,
+    internal,
+    notImplemented,
+    phorohphor,
 };
--- src/server.js
+++ src/server.js
@@ -7,103 +7,99 @@ const xmlHandler = require('./xmlResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
+    const parsedUrl = url.parse(request.url);
 
-  console.dir(parsedUrl);
-  console.dir(request);
-  console.dir(response);
+    if (request.headers.accept === 'text/xml') {
+        switch (parsedUrl.pathname) {
+            case '/style.css':
+                htmlHandler.getCSS(request, response);
+                break;
 
-  if (request.accept === 'text/xml') {
-    switch (parsedUrl.pathname) {
-      case '/style.css':
-        htmlHandler.getCSS(request, response);
-        break;
+            case '/client.html':
+                htmlHandler.getIndex(request, response);
+                break;
 
-      case '/client.html':
-        htmlHandler.getIndex(request, response);
-        break;
+            case '/favicon.ico':
+                break;
 
-      case '/favicon.ico':
-        break;
+            case '/':
+                htmlHandler.getIndex(request, response);
+                break;
 
-      case '/':
-        htmlHandler.getIndex(request, response);
-        break;
+            case '/success':
+                xmlHandler.success(request, response);
+                break;
 
-      case '/success':
-        xmlHandler.success(request, response);
-        break;
+            case '/badRequest':
+                xmlHandler.badRequest(request, response, parsedUrl.query);
+                break;
 
-      case '/badRequest':
-        xmlHandler.badRequest(request, response, parsedUrl.query);
-        break;
+            case '/unauthorized':
+                xmlHandler.unauthorized(request, response, parsedUrl.query);
+                break;
 
-      case '/unauthorized':
-        xmlHandler.unauthorized(request, response, parsedUrl.query);
-        break;
+            case '/forbidden':
+                xmlHandler.forbidden(request, response);
+                break;
 
-      case '/forbidden':
-        xmlHandler.forbidden(request, response);
-        break;
+            case '/internal':
+                xmlHandler.internal(request, response);
+                break;
 
-      case '/internal':
-        xmlHandler.internal(request, response);
-        break;
+            case '/notImplemented':
+                xmlHandler.notImplemented(request, response);
+                break;
 
-      case '/notImplemented':
-        xmlHandler.notImplemented(request, response);
-        break;
+            default:
+                xmlHandler.phorohphor(request, response);
+                break;
+        }
+    } else {
+        switch (parsedUrl.pathname) {
+            case '/style.css':
+                htmlHandler.getCSS(request, response);
+                break;
 
-      default:
-        xmlHandler.phorohphor(request, response);
-        break;
+            case '/client.html':
+                htmlHandler.getIndex(request, response);
+                break;
+
+            case '/favicon.ico':
+                break;
+
+            case '/':
+                htmlHandler.getIndex(request, response);
+                break;
+
+            case '/success':
+                jsonHandler.success(request, response);
+                break;
+
+            case '/badRequest':
+                jsonHandler.badRequest(request, response, parsedUrl.query);
+                break;
+
+            case '/unauthorized':
+                jsonHandler.unauthorized(request, response, parsedUrl.query);
+                break;
+
+            case '/forbidden':
+                jsonHandler.forbidden(request, response);
+                break;
+
+            case '/internal':
+                jsonHandler.internal(request, response);
+                break;
+
+            case '/notImplemented':
+                jsonHandler.notImplemented(request, response);
+                break;
+
+            default:
+                jsonHandler.phorohphor(request, response);
+                break;
+        }
     }
-  } else {
-    switch (parsedUrl.pathname) {
-      case '/style.css':
-        htmlHandler.getCSS(request, response);
-        break;
-
-      case '/client.html':
-        htmlHandler.getIndex(request, response);
-        break;
-
-      case '/favicon.ico':
-        break;
-
-      case '/':
-        htmlHandler.getIndex(request, response);
-        break;
-
-      case '/success':
-        jsonHandler.success(request, response);
-        break;
-
-      case '/badRequest':
-        jsonHandler.badRequest(request, response, parsedUrl.query);
-        break;
-
-      case '/unauthorized':
-        jsonHandler.unauthorized(request, response, parsedUrl.query);
-        break;
-
-      case '/forbidden':
-        jsonHandler.forbidden(request, response);
-        break;
-
-      case '/internal':
-        jsonHandler.internal(request, response);
-        break;
-
-      case '/notImplemented':
-        jsonHandler.notImplemented(request, response);
-        break;
-
-      default:
-        jsonHandler.phorohphor(request, response);
-        break;
-    }
-  }
 };
 
 http.createServer(onRequest).listen(port);
